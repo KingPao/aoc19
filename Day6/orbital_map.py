14fa38c5799272f64ff62c4e698203255b43b609
Geringer <jakob.geringer@accenture.com>
2019-12-16 09:33:03 +0100

    add day6

---
+++ Day6/orbital_map.py
@@ -0,0 +1,35 @@
+data_input = ["COM)B",
+"B)C",
+"C)D",
+"D)E",
+"E)F",
+"B)G",
+"G)H",
+"D)I",
+"E)J",
+"J)K",
+"K)L"]
+
+ 
+class Orbital_Map(object):
+    connections = []
+
+    def dummy(self):
+        com = "tba"
+        for orbit_relation in data_input:
+            orbits = orbic_relation.split(")")
+
+            while True:
+                if find_com:
+                    starting_point = predecessor
+                    break
+
+            predecessor = orbtis[0]
+            successor   = orbits[1]
+
+    def find_com(self, predecessor):
+
+        if predecessor == "COM":
+            return True
+
+        return False
